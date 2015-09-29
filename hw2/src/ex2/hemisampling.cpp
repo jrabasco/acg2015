1e3ba4ec9846380988246e77d37c2fa13e82e671
philemonf <philemon.favrod@epfl.ch>
2015-09-29 18:17:45 +0200

    Implements squareToCosineHemisphere

--- hw2/src/ex2/hemisampling.cpp
+++ hw2/src/ex2/hemisampling.cpp
@@ -17,9 +17,16 @@ NORI_NAMESPACE_BEGIN
  * \param sample a 2d uniform sample
  */
 inline Vector3f squareToCosineHemisphere(const Point2f &sample) {
+    // At first, we generate a random point on a sphere
+    float randomRadius = sqrtf(sample.x());
+    float randomAngle = 2 * M_PI * sample.y();
 
-        // TODO implement cosine hemisphere sampling using a given uniform [0;1]^2 sample
-        return Vector3f(0.0f, 0.0f, 1.0f); // this is wrong, replace it with your solution!
+    // We apply pythagore knowing that sqrt(sample.x())^2 = sample.x()...
+    float zCoordinate = sqrtf(1 - sample.x());
+
+    return Vector3f(randomRadius * cosf(randomAngle),
+                    randomRadius * sinf(randomAngle),
+                    zCoordinate);
 }
 
 NORI_NAMESPACE_END
