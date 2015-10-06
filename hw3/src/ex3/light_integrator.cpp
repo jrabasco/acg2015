870ef4d748f86cdca1298ff04214d8c767380bd5
Solal Pirelli <solal.pirelli@gmail.com>
2015-10-06 18:58:21 +0200

    [3] Use BSDFQueryRecord and eval.

--- hw3/src/ex3/light_integrator.cpp
+++ hw3/src/ex3/light_integrator.cpp
@@ -117,7 +117,13 @@ public:
                 Color3f sampledLight = sampleLights(scene, lRec, sample);
 
                 float cosThetaPrime = its.shFrame.n.dot(lRec.d);
-                return sampledLight * cosThetaPrime;
+                const BSDFQueryRecord bsdfRec;
+                bsdfRec.wo=ray.d;
+                bsdfRec.wi=lRec.d;
+                bsdfRec.eta=1;
+                bsdfRec.measure=ESolidAngle;
+
+                return bsdf->eval(bsdfRec) * sampledLight * cosThetaPrime;
         }
 
         QString toString() const {
